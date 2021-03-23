3fa99163a74f2403c8b649af444de91b7fe82f25
LongHoang2205 <hoangvulong2205@gmail.com>
2021-03-23 20:10:23 +0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View , SafeAreaView,TextInput} from 'react-native';
+import {RNButton, RNTextInput,RNButtonSubmit} from './components';
+import Styles from './AppStyle'
+// import styles from './components/TextInput/TextInputStyle';
+// import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={Styles.container}>
+      <View style={Styles.title}>
+          <Text style={Styles.text}>
+            Sign In
+          </Text>
+      </View>
+      <View style={Styles.input}> 
+            <RNTextInput placeholder={'username'} />
+            <RNTextInput placeholder={'password'} />
+      </View>
+
+      <View style={Styles.validate}>
+        <RNButton style={Styles.btn} textButton={"Fogot"} nameOfButton={"Username / Password?"} />
+      </View>
+      <View>
+      <RNButtonSubmit/>
+      </View>
+      
+      <View style={Styles.denied} >
+          <RNButton nameOfButton={"Don't have an account"} />
+      </View>
+      <View style={Styles.signUp} >
+          <RNButton nameOfButton={"SIGN UP NOW"} />
+      </View>
+     
+        
+      
+</SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
